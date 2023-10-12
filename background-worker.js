720d6a95051f85e71025453048ba3c0de07cdf0b
DarwiinSA <146110512+DarwiinSA@users.noreply.github.com>
2023-10-12 15:00:44 +0200

    Update background-worker.js V2

--- background-worker.js
+++ background-worker.js
@@ -1,7 +1,210 @@
 let tabStates = {};
-let timers = {};
 
-function getSoonestEndingTimer() {
+function startTimer(timerId, duration, callback) {
+    const startTime = Date.now();
+
+    function tick() {
+        chrome.storage.local.get(['timers'], function(data) {
+            const timers = data.timers;
+
+            if (!timers || !timers[timerId]) {
+                return;
+            }
+
+            const elapsed = Date.now() - startTime;
+            const remaining = duration - elapsed;
+
+            const minutes = Math.floor(remaining / 60000);
+            const seconds = ((remaining % 60000) / 1000).toFixed(0);
+            const displayTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+            const timerName = timers[timerId].name;
+
+            chrome.runtime.sendMessage({
+                action: "updateTimer", 
+                time: displayTime, 
+                timerId: timerId,
+                name: timerName
+            });
+
+            if (remaining <= 0) {
+                callback();
+            } else {
+                setTimeout(tick, Math.min(1000, remaining));
+            }
+        });
+    }
+
+    tick();
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({ isActive: false, timers: {} });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'loading' && tab.url && tab.url.startsWith("https://labs.staratlas.com/")) {
+        delete tabStates[tabId];
+        chrome.storage.local.set({ isActive: false }, function() {
+            chrome.action.setBadgeText({ text: "", tabId: tabId });
+            chrome.storage.local.get(['timers'], function(data) {
+                updateBadgeText(data.timers);
+                startBadgeUpdater();
+            });
+        });
+    }
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    switch (request.action) {
+        case "activate":
+            handleActivate(request, sendResponse);
+            break;
+        case "deactivate":
+            handleDeactivate(request, sendResponse);
+            break;
+        case "checkStatus":
+            chrome.storage.local.get(['isActive'], function(data) {
+                sendResponse({status: data.isActive ? "Active" : "Inactive"});
+            });
+            break;
+        case "startTimer":
+            handleStartTimer(request, sendResponse);
+            break;
+        case "getTimers":
+            handleGetTimers(request, sendResponse);
+            break;
+        default:
+            sendResponse({ status: "unknown" });
+            break;
+        case "resetTimers":
+            handleResetTimers(sendResponse);
+            break;
+    }
+
+    return true; // Keep the message channel open for asynchronous responses
+});
+
+function handleActivate(request, sendResponse) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        const tabId = tabs[0].id;
+        const tabUrl = tabs[0].url;
+
+        if (!tabUrl.startsWith("https://labs.staratlas.com/")) {
+            sendResponse({status: "Wrong Site"});
+            return;
+        }
+
+        chrome.storage.local.set({ isActive: true });
+
+        if (!tabStates[tabId]) {
+            chrome.scripting.executeScript({
+                target: {tabId: tabId},
+                files: ['content.js']
+            }, () => {
+                tabStates[tabId] = true;
+                chrome.tabs.sendMessage(tabId, { action: "activate" });
+            });
+        } else {
+            chrome.tabs.sendMessage(tabId, { action: "activate" });
+        }
+
+        chrome.action.setBadgeText({text: "ON", tabId: tabId});
+        chrome.action.setBadgeBackgroundColor({color: "#efad45", tabId: tabId});
+        sendResponse({status: "Active"});
+    });
+}
+
+
+function handleDeactivate(request, sendResponse) {
+    chrome.storage.local.set({ isActive: false });
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        const tabId = tabs[0].id;
+        chrome.action.setBadgeText({text: "", tabId: tabId});
+        chrome.tabs.sendMessage(tabId, { action: "deactivate" });
+    });
+    sendResponse({status: "Inactive"});
+}
+
+function handleStartTimer(request, sendResponse) {
+    const duration = request.duration * 1000;
+    const timerId = Date.now();
+
+    chrome.storage.local.get(['timers'], function(data) {
+        let timers = data.timers || {};
+        timers[timerId] = {
+            startTime: Date.now(),
+            duration: duration,
+            name: request.name
+        };
+
+        chrome.storage.local.set({ timers: timers }, startBadgeUpdater);
+
+        startTimer(timerId, duration, () => {
+            chrome.storage.local.get(['timers'], function(data) {
+                let updatedTimers = data.timers;
+                if (updatedTimers && updatedTimers[timerId]) {
+                    delete updatedTimers[timerId];
+                    chrome.storage.local.set({ timers: updatedTimers }, function() {
+                        chrome.storage.local.get(['timers'], function(data) {
+                            updateBadgeText(data.timers);
+                        });
+                    });
+                }
+            });
+            chrome.runtime.sendMessage({action: "updateTimer", time: "00:00", timerId: timerId});
+        });
+    });
+}
+
+function handleGetTimers(request, sendResponse) {
+    chrome.storage.local.get(['timers'], function(data) {
+        let timers = data.timers || {};
+        const responseTimers = {};
+        for (let timerId in timers) {
+            const elapsed = Date.now() - timers[timerId].startTime;
+            const remaining = timers[timerId].duration - elapsed;
+            const minutes = Math.floor(remaining / 60000);
+            const seconds = ((remaining % 60000) / 1000).toFixed(0);
+            const displayTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+            responseTimers[timerId] = {time: displayTime, name: timers[timerId].name};
+        }
+        sendResponse({timers: responseTimers});
+    });
+}
+
+let badgeUpdaterInterval;
+
+function startBadgeUpdater() {
+    if (badgeUpdaterInterval) {
+        clearInterval(badgeUpdaterInterval);
+    }
+
+    badgeUpdaterInterval = setInterval(() => {
+        chrome.storage.local.get(['timers'], function(data) {
+            updateBadgeText(data.timers);
+        });
+    }, 1000);
+}
+
+function updateBadgeText(timers) {
+    const soonestTimer = getSoonestEndingTimer(timers);
+    if (soonestTimer) {
+        const elapsedSoonest = Date.now() - soonestTimer.startTime;
+        const remainingSoonest = soonestTimer.duration - elapsedSoonest;
+        if (remainingSoonest <= 1000) {
+            chrome.action.setBadgeText({text: "-"});
+        } else {
+            const minutesSoonest = Math.floor(remainingSoonest / 60000);
+            const secondsSoonest = ((remainingSoonest % 60000) / 1000).toFixed(0);
+            const badgeDisplayTime = `${minutesSoonest}:${secondsSoonest < 10 ? '0' : ''}${secondsSoonest}`;
+            chrome.action.setBadgeText({text: badgeDisplayTime});
+        }
+    } else {
+        chrome.action.setBadgeText({text: "-"});
+    }
+}
+
+function getSoonestEndingTimer(timers) {
     let soonestTimer = null;
     let leastTimeRemaining = Infinity;
 
@@ -17,112 +220,37 @@ function getSoonestEndingTimer() {
     return soonestTimer;
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-    // Set initial state
-    chrome.storage.local.set({ isActive: false });
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'loading' && tab.url && tab.url.startsWith("https://labs.staratlas.com/")) {
-        // Reset state for the tab on refresh
-        tabStates[tabId] = false;
-        chrome.storage.local.set({ isActive: false });
-        chrome.action.setBadgeText({ text: "", tabId: tabId });
-    }
-});
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action == "activate") {
-        chrome.storage.local.get(['isActive'], function (result) {
-            if (!result.isActive) {
-                chrome.storage.local.set({ isActive: true });
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    let tabId = tabs[0].id;
-                    if (!tabStates[tabId]) {
-                        chrome.scripting.executeScript({
-                            target: { tabId: tabId },
-                            files: ['content.js']
-                        }, () => {
-                            tabStates[tabId] = true;
-                            chrome.tabs.sendMessage(tabId, { action: "activate" });
-                        });
-                    } else {
-                        chrome.tabs.sendMessage(tabId, { action: "activate" });
-                    }
-                    chrome.action.setBadgeText({ text: "ON", tabId: tabId });
-                    chrome.action.setBadgeBackgroundColor({ color: "#0A74DA", tabId: tabId });
-                });
-                sendResponse({ status: "Active" });
-            }
-        });
-    } else if (request.action == "deactivate") {
-        chrome.storage.local.get(['isActive'], function (result) {
-            if (result.isActive) {
-                chrome.storage.local.set({ isActive: false });
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    let tabId = tabs[0].id;
-                    chrome.action.setBadgeText({ text: "", tabId: tabId });
-                    chrome.tabs.sendMessage(tabId, { action: "deactivate" });
-                });
-                sendResponse({ status: "Inactive" });
-            }
-        });
-    } else if (request.action == "checkStatus") {
-        chrome.storage.local.get(['isActive'], function (result) {
-            sendResponse({ status: result.isActive ? "Active" : "Inactive" });
-        });
-    } else if (request.action == "startTimer") {
-        let duration = request.duration * 1000;
-        let timerId = Date.now();
-        timers[timerId] = {
-            startTime: Date.now(),
-            duration: duration,
-            name: request.name,
-            intervalId: null
-        };
-
-        timers[timerId].intervalId = setInterval(() => {
-            let elapsed = Date.now() - timers[timerId].startTime;
-            let remaining = timers[timerId].duration - elapsed;
-            if (remaining <= 0) {
-                clearInterval(timers[timerId].intervalId);
-                delete timers[timerId];
-                chrome.runtime.sendMessage({ action: "updateTimer", time: "00:00", timerId: timerId });
-            } else {
-                let minutes = Math.floor(remaining / 60000);
-                let seconds = ((remaining % 60000) / 1000).toFixed(0);
-                let displayTime = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-                chrome.runtime.sendMessage({ action: "updateTimer", time: displayTime, timerId: timerId, name: timers[timerId].name });
-            }
-
-            // Update the badge text with the time of the soonest ending timer
-            let soonestTimer = getSoonestEndingTimer();
-            if (soonestTimer) {
-                let elapsedSoonest = Date.now() - soonestTimer.startTime;
-                let remainingSoonest = soonestTimer.duration - elapsedSoonest;
-                if (remainingSoonest <= 1000) {
-                    chrome.action.setBadgeText({ text: "-" });
-                } else {
-                    let minutesSoonest = Math.floor(remainingSoonest / 60000);
-                    let secondsSoonest = ((remainingSoonest % 60000) / 1000).toFixed(0);
-                    let badgeDisplayTime = minutesSoonest + ":" + (secondsSoonest < 10 ? '0' : '') + secondsSoonest;
-                    chrome.action.setBadgeText({ text: badgeDisplayTime });
-                }
-            }
-
-        }, 1000);
-    } else if (request.action == "getTimers") {
-        let responseTimers = {};
+function cleanupElapsedTimers() {
+    chrome.storage.local.get(['timers'], function(data) {
+        let timers = data.timers || {};
+        const currentTime = Date.now();
         for (let timerId in timers) {
-            let elapsed = Date.now() - timers[timerId].startTime;
-            let remaining = timers[timerId].duration - elapsed;
-            let minutes = Math.floor(remaining / 60000);
-            let seconds = ((remaining % 60000) / 1000).toFixed(0);
-            let displayTime = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-            responseTimers[timerId] = { time: displayTime, name: timers[timerId].name };
+            const elapsed = currentTime - timers[timerId].startTime;
+            if (elapsed >= timers[timerId].duration) {
+                delete timers[timerId];
+            }
         }
-        sendResponse({ timers: responseTimers });
-    }
+        chrome.storage.local.set({ timers: timers });
+    });
+}
 
-    return true;
-});
+function handleResetTimers(sendResponse) {
+    chrome.storage.local.set({ timers: {} }, function() {
+        if (chrome.runtime.lastError) {
+            sendResponse({ status: "error", message: chrome.runtime.lastError.message });
+        } else {
+            // Update badge text
+            chrome.action.setBadgeText({text: "-"});
+            // Stop any ongoing badge updater
+            if (badgeUpdaterInterval) {
+                clearInterval(badgeUpdaterInterval);
+            }
+            // Send a message to the content script to clear the timeouts
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                const tabId = tabs[0].id;
+                chrome.tabs.sendMessage(tabId, { action: "resetTimers" });
+            });
+            sendResponse({ status: "reset" });
+        }
+    });
+}
